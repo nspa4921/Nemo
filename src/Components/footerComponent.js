066696a0a7fed62c029972c7e0a8c0c6b37d8edf
Nemanja Spasic <nemo.spaske@live.com>
2020-08-02 21:46:06 +0200

    footer fixed

--- src/Components/footerComponent.js
+++ src/Components/footerComponent.js
@@ -5,12 +5,12 @@ function Footer(props) {
     return(
         <Container className="wrapper-right">
             <Row>
-              <Col xs="6" sm="6">  
+              <Col xs="3" sm="6">  
                     <div className="footer" align="left">
                         <p>@ Nemo 2020</p>
                     </div>
                 </Col>
-                <Col xs="12" sm="6">
+                <Col xs="9" sm="6">
                     <div className="footer" align="right">
                         <a href="#top" class="top">SCROLL TO TOP</a> / <a class="contact_link" href="#contact">CONTACT</a>
                     </div>
