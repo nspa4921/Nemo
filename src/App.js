2b9a9be87c18e88fd5a810d15cf52850b0587985
Nemanja Spasic <59572186+nspa4921@users.noreply.github.com>
2021-12-07 14:16:04 +0100

    Particles imported

--- src/App.js
+++ src/App.js
@@ -2,17 +2,63 @@ import React, {Component} from 'react';
 import './App.css';
 import "animate.css/animate.min.css";
 import Main from './Components/mainComponent';
+import Particles from "react-tsparticles";
 
+const App = () => {
+  const particlesInit = (main) => {
+    console.log(main);
 
-class App extends Component {
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
 
-  render() {
-    return (
-      <div className="App">
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+  return (
+    <>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          preset: "big-circles",
+          backgroundMask: {
+            enable: true,
+            cover: {
+              value: {
+                r: 0,
+                g: 0,
+                b: 0
+              }
+            }
+          },
+          background: {
+            image:
+              "url('https://hdwallsource.com/img/2014/4/digital-wallpapers-24971-25651-hd-wallpapers.jpg')",
+            size: "100% 100%",
+            repeat: "no-repeat"
+          },
+          particles: {
+            opacity: {
+              value: 1,
+              random: {
+                enable: true,
+                minimumValue: 0.05
+              },
+              animation: {
+                enable: true,
+                speed: 1.5,
+                minimumValue: 0.05
+              }
+            },
+            size: {
+              value: 150
+            }
+          }
+          }} />
         <Main />
-      </div>
-    );
-  }
-}
+       </>
+  );
+};
 
 export default App;
